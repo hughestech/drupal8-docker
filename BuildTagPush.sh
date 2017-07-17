ae21b81fca1b2053b0099f7f409ec156709ef871
anton.hughes <kurrent93@gmail.com>
2017-07-17 17:09:18 +0700

    using minishift-centos7

--- BuildTagPush.sh
+++ BuildTagPush.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TAG=hughestech/lightning
+TAG=hughestech/opensocial
 
 
 set -ex
@@ -11,6 +11,8 @@ IMAGE_NAME="$TAG"
 
 
 REGISTRY="$(minishift openshift registry)"
+
+minishift start --openshift-version=v1.5.0 --iso-url https://github.com/minishift/minishift-centos-iso/releases/download/v1.0.0/minishift-centos7.iso
 
 eval $(minishift docker-env)
 docker login -u developer -p $(oc whoami -t) ${REGISTRY}
